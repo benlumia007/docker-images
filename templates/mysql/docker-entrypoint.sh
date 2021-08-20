9ee6ad2b9731fc5d11da14452deab37769208470
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-08-20 09:47:44 -0700

    this should help reduce some unnecesary stuff

--- templates/mysql/docker-entrypoint.sh
+++ templates/mysql/docker-entrypoint.sh
@@ -1,95 +1,6 @@
 #!/bin/bash
 
-# arguments necessary to run "mysqld --verbose --help" successfully (used for testing configuration validity and for extracting default/configured values)
-_verboseHelpArgs=(
-	--verbose --help
-	--log-bin-index="$(mktemp -u)" # https://github.com/docker-library/mysql/issues/136
-)
-
-# Fetch value from server config
-# We use mysqld --verbose --help instead of my_print_defaults because the
-# latter only show values present in config files, and not server defaults
-mysql_get_config() {
-	local conf="$1"; shift
-	"$@" "${_verboseHelpArgs[@]}" 2>/dev/null \
-		| awk -v conf="$conf" '$1 == conf && /^[^ \t]/ { sub(/^[^ \t]+[ \t]+/, ""); print; exit }'
-	# match "datadir      /some/path with/spaces in/it here" but not "--xyz=abc\n     datadir (xyz)"
-}
-
-# Do a temporary startup of the MySQL server, for init purposes
-docker_temp_server_start() {
-	# For 5.7+ the server is ready for use as soon as startup command unblocks
-	if ! "$@" --daemonize --skip-networking --default-time-zone=SYSTEM --socket="${SOCKET}"; then
-		mysql_error "Unable to start server."
-	fi
-}
-
-# Stop the server. When using a local socket file mysqladmin will block until
-# the shutdown is complete.
-docker_temp_server_stop() {
-	if ! mysqladmin --defaults-extra-file=<( _mysql_passfile ) shutdown -uroot --socket="${SOCKET}"; then
-		mysql_error "Unable to shut down server."
-	fi
-}
-
-# creates folders for the database
-# also ensures permission for user mysql of run as root
-docker_create_db_directories() {
-	local user; user="$(id -u)"
-
-	# TODO other directories that are used by default? like /var/lib/mysql-files
-	# see https://github.com/docker-library/mysql/issues/562
-	mkdir -p "$DATADIR"
-
-	if [ "$user" = "0" ]; then
-		# this will cause less disk access than `chown -R`
-		find "$DATADIR" \! -user mysql -exec chown mysql '{}' +
-	fi
-}
-
-# initializes the database directory
-docker_init_database_dir() {
-	"$@" --initialize-insecure --default-time-zone=SYSTEM
-}
-
-# Loads various settings that are used elsewhere in the script
-# This should be called after mysql_check_config, but before any other functions
-docker_setup_env() {
-	# Get config
-	declare -g DATADIR SOCKET
-	DATADIR="$(mysql_get_config 'datadir' "$@")"
-	SOCKET="$(mysql_get_config 'socket' "$@")"
-
-	declare -g DATABASE_ALREADY_EXISTS
-	if [ -d "$DATADIR/mysql" ]; then
-		DATABASE_ALREADY_EXISTS='true'
-	fi
-}
-
-docker_process_sql() {
-	passfileArgs=()
-	if [ '--dont-use-mysql-root-password' = "$1" ]; then
-		passfileArgs+=( "$1" )
-		shift
-	fi
-	mysql --defaults-extra-file=<( _mysql_passfile "${passfileArgs[@]}") --protocol=socket -uroot -hlocalhost --socket="${SOCKET}" --comments "$@"
-}
-
-# Initializes database with timezone info and root password, plus optional extra db/user
-docker_setup_db() {
-	# tell docker_process_sql to not use MYSQL_ROOT_PASSWORD since it is just now being set
-	docker_process_sql --dont-use-mysql-root-password --database=mysql <<-EOSQL
-		ALTER USER 'root'@'localhost' IDENTIFIED BY 'root' ;
-		GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION ;
-		FLUSH PRIVILEGES ;
-		DROP DATABASE IF EXISTS test ;
-	EOSQL
-}
-
 _mysql_passfile() {
-	# echo the password to the "file" the client uses
-	# the client command will use process substitution to create a file on the fly
-	# ie: --defaults-extra-file=<( _mysql_passfile )
 	if [ '--dont-use-mysql-root-password' != "$1" ] && [ -n "root" ]; then
 		cat <<-EOF
 			[client]
@@ -98,27 +9,62 @@ _mysql_passfile() {
 	fi
 }
 
-# check arguments for an option that would cause mysqld to stop
-# return true if there is one
-_mysql_want_help() {
-	local arg
-	for arg; do
-		case "$arg" in
-			-'?'|--help|--print-defaults|-V|--version)
-				return 0
-				;;
-		esac
-	done
-	return 1
+# Here, we are going to setup some variables for later use. This allows us to use
+# variables anywhere.
+setup_environment_variables() {
+	# declare a variable for data_directory and socket
+	declare -g data_directory socket data_directory_exists
+
+	# setup variable with data
+	data_directory="var/lib/mysql"
+	socket="/var/run/mysqld/mysqld.sock"
+
+	if [[ -d "${data_directory}/mysql" ]]; then
+		data_directory_exists='true'
+	fi
 }
 
-_main() {
+# Here, we are going to initializes the database directory
+initialize_database_directory() {
+	"$@" --initialize-insecure --default-time-zone=SYSTEM
+}
+
+# Here, we are going to make sure that the root password is set up properly
+processing_sql() {
+	mysql --protocol=socket -uroot -hlocalhost --socket="${socket}"
+}
+
+# Initializes database with timezone info and root password, plus optional extra db/user
+setup_database() {
+	processing_sql --dont-use-mysql-root-password --database=mysql <<-EOSQL
+		ALTER USER 'root'@'localhost' IDENTIFIED BY 'root' ;
+		GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION ;
+		FLUSH PRIVILEGES ;
+		DROP DATABASE IF EXISTS test ;
+	EOSQL
+}
+
+# Do a temporary startup of the MySQL server, for init purposes
+start_server() {
+	if ! "$@" --daemonize --skip-networking --default-time-zone=SYSTEM --socket="${socket}"; then
+		echo "Unable to start server."
+	fi
+}
+
+# Stop the server. When using a local socket file mysqladmin will block until
+# the shutdown is complete.
+stop_server() {
+	if ! mysqladmin --defaults-extra-file=<( _mysql_passfile ) shutdown -uroot --socket="${socket}"; then
+		echo "Unable to shut down server."
+	fi
+}
+
+start() {
 	# skip setup if they aren't running mysqld or want an option that stops mysqld
-	if [ "$1" = 'mysqld' ] && ! _mysql_want_help "$@"; then
+	if [[ "$1" = 'mysqld' ]]; then
 	
 		# Load various environment variables
-		docker_setup_env "$@"
-		docker_create_db_directories
+		setup_environment_variables "$@"
 
 		# If container is started as root user, restart as dedicated mysql user
 		if [ "$(id -u)" = "0" ]; then
@@ -126,18 +72,18 @@ _main() {
 		fi
 
 		# there's no database, so it needs to be initialized
-		if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
+		if [ -z "${data_directory_exists}" ]; then
 
-			docker_init_database_dir "$@"
+			initialize_database_directory "$@"
 
-			docker_temp_server_start "$@"
+			start_server "$@"
 
-			docker_setup_db
+			setup_database
 
-			docker_temp_server_stop
+			stop_server
 		fi
 	fi
 	exec "$@"
 }
 
-_main "$@"
+start "$@"
